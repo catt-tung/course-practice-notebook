92d8d3f9a4b6838dbeea0930af610f13ee4cec7f
Catherine Tung <catt.tung@gmail.com>
2023-06-05 12:42:26 -0400

    everyday backpack and console logging

---
+++ 03_10/script.js
@@ -0,0 +1,14 @@
+import Backpack from "./Backpack";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false
+);
+
+console.log("The everydayBackpack objectL", everydayPack);
+console.log("pocketNum value:", everydayPack.pocketNum)
