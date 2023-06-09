0c6551b6f268ffde09cf8e890ffcbdf99bf0599f
Catherine Tung <catt.tung@gmail.com>
2023-06-08 23:24:13 -0400

    create objects from classes

---
+++ js-essential-training/03_12/script.js
@@ -0,0 +1,20 @@
+import Backpack from "./Backpack.js";
+import WaterGlass from "./WaterGlass.js";
+
+const paperlessBackpack = new Backpack (
+  'Paperless Backpack',
+  30,
+  'black',
+  6,
+  26,
+  26,
+  true
+)
+
+const cocktailGlass = new WaterGlass (
+  'Cocktail Glass',
+  300,
+  8,
+  'Cocktails',
+  false
+)
