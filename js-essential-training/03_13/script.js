2c836a86a71bc80dc2e18e98f0a012f8cb735dcf
Catherine Tung <catt.tung@gmail.com>
2023-06-13 23:49:57 -0400

    script for creating a new backpack

---
+++ js-essential-training/03_13/script.js
@@ -0,0 +1,12 @@
+import Backpack from "./Backpack.js";
+
+const everydayPack = new Backpack(
+  'Everyday Backpack',
+  30,
+  'grey',
+  15,
+  26,
+  26,
+  false,
+  'December 5, 2020 15:00:00 EST'
+)
