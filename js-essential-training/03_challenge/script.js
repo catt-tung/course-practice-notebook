89f4a7466ad25015f07adb299c9a4da66b910c07
Catherine Tung <catt.tung@gmail.com>
2023-06-18 17:12:15 -0400

    add fluent python book as object

---
+++ js-essential-training/03_challenge/script.js
@@ -0,0 +1,14 @@
+import Book from "./Book.js";
+
+const fluentPythonBook = new Book(
+  "Fluent Python",
+  "Luciano Ramalho",
+  2022,
+  "USA",
+  "Non-fiction",
+  983,
+  0,
+  983
+);
+
+console.log("Fluent Python Book:", fluentPythonBook);
